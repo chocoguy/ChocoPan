4e910da71595fa0757f51b4f596beb6501f48543
Edgar Zarco <chocoguy@mailfence.com>
2026-09-16 19:57:08 -0500

    Initial Commit

---
+++ ChocoPan/ChocoPanApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChocoPanApp.swift
+//  ChocoPan
+//
+//  Created by Edgar Zarco on 9/16/26.
+//
+
+import SwiftUI
+
+@main
+struct ChocoPanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
